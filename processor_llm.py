47d89492c93b505933312dc0090c7e2a8f7da0b0
AKASH K <akashcbe46@gmail.com>
2026-04-20 13:49:38 +0530

    Update processor_llm.py

--- processor_llm.py
+++ processor_llm.py
@@ -23,7 +23,7 @@ def classify_with_llm(log_msg):
     chat_completion = groq.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
         # model="llama-3.3-70b-versatile",
-        model="deepseek-r1-distill-llama-70b",
+        model="llama-3.3-70b-versatile",
         temperature=0.5
     )
 
